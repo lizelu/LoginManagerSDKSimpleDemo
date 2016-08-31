bccde3ecbe375f69dd67c386f62c1995f410c26f
zeluliCredit <zeluli@creditease.cn>
2016-08-31 09:39:23 +0800

    commit sdk base project

---
+++ AccountManagerSDKResource/AccountManagerSDKResource/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AccountManagerSDKResource
+//
+//  Created by Mr.LuDashi on 16/8/31.
+//  Copyright © 2016年 ZeluLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AccountManagerSDKResource/AccountManagerSDKResource/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccountManagerSDKResource
+//
+//  Created by Mr.LuDashi on 16/8/31.
+//  Copyright © 2016年 ZeluLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
